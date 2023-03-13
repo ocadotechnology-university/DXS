34efbf53183dd1c2f0cb9c947c06d260bb311461
Sharrat <mrkaczucha@gmail.com>
2023-03-13 16:05:56 +0100

    Added initial code base. Added "hello world" controller implementation.

---
+++ src/test/java/com/piwo/DXS/DxsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.piwo.DXS;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DxsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

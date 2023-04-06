f7d2a84c5b09a2d7fabf62fa99266f8778ab47e1
kuniuu <jakub.kuniewski@gmail.com>
2023-04-06 23:01:50 +0200

    Centered text in test page.

--- src/main/webapp/app/modules/test/test-website.tsx
+++ src/main/webapp/app/modules/test/test-website.tsx
@@ -5,10 +5,10 @@ export const TestWebsite = () => {
   return (
     <>
       <Row>
-        <h2>Here you can add your mockup implementation. Good luck :)</h2>
+        <h2 style={{ textAlign: 'center' }}>Here you can add your mockup implementation. Good luck :)</h2>
       </Row>
       <Row>
-        <h3>Remember to change "TestWebsite" module and header component to the proper name</h3>
+        <h3 style={{ textAlign: 'center' }}>Remember to change "TestWebsite" module and header component to the proper name</h3>
       </Row>
     </>
   );
